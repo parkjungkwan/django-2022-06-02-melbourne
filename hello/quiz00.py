08d7f373f0ea90267c8a7b5b6068f520e0c55b28
parkjungkwan <pakjkwan@gmail.com>
2022-03-04 14:40:25 +0900

    14

--- hello/quiz00.py
+++ hello/quiz00.py
@@ -1,38 +1,135 @@
-if __name__ == '__main__':
-    while 1:
-        menu = input('"00계산기 01Bmi 02주사위 03가위바위보 04윤년 05숫자추론 06로또 07예약 08입출금 09구구단"')
-        if menu == '0':
-            break
-        elif menu == '1':
-            q1 = Quiz01Calculator(int(input('첫번째 수')), int(input('두번째 수')))
-            print(f'{q1.num1} + {q1.num2} = {q1.add()}')
-        elif menu == '2':
-            member = Member()
-            q2 = Quiz02Bmi()
-            member.name = input('이름 : ')
-            member.height = float(input('키 : '))
-            member.weight = float(input('몸무게 : '))
-            res = q2.getBmi(member)
-            print(f'이름: {member.name}, 키: {member.height}, '
-                  f'몸무게: {member.weight}, BMI상태: {res} ')
-        elif menu == '3':
-            q3 = Quiz03Grade()
-        elif menu == '4':
-            q4 = Quiz04GradeAuto()
-            for i in ['김유신', '강감찬', '유관순', '윤봉길', '신사임당']:
-                print(i)
-            kor = int(input('국어 : '))
-            eng = int(input('영어 : '))
-            math = int(input('수학 : '))
-            # grade =Grade(name,kor,eng,math)
-            # print(f'{name}님의 국어{kor} 영어{eng} 수학{math} 합계{grade.sum()} 평균{grade.avg()}')
-        elif menu == '5':
-            print(Quiz05Dice.cast())
-        elif menu == '6':
-            q6 = None
-        elif menu == '7':
-            q7 = Quiz07RandomChoice()
-            print(q7.chooseMember())
-        elif menu == '8':
-            q8 = Quiz08Rps(1) # 가위 1 바위 2 보 3
-            print(q8.game())
+from hello import Member
+from hello.domains import my100, myRandom
+
+
+class Quiz00:
+    def quiz00calculator(self)-> float:
+        a = my100()
+        b = my100()
+        return self.add(a, b)
+
+    def add(self, a, b) -> float:
+        return a + b
+
+    def sub(self, a, b) -> float:
+        return a - b
+
+    def mul(self, a, b) -> float:
+        return a * b
+
+    def div(self, a, b) -> float:
+        return a / b
+
+    def mod(self, a, b) -> float:
+        return a % b
+
+    def quiz01bmi(self):
+        this = Member()
+        this.name = '홍길동'
+        this.height = 170.8
+        this.weight = 120.8
+        res = this.weight / (this.height * this.height) * 10000
+        if res > 25:
+            return f'비만'
+        elif res > 23:
+            return f'과체중'
+        elif res > 18.5:
+            return f'정상'
+        else:
+            return f'저체중'
+
+    def quiz02dice(self):
+        return myRandom(1, 6)
+
+    def quiz03rps(self):
+        c = myRandom(1, 3)
+        p = input('가위', '바위', '보')
+        # 1 가위 2  바위 3 보
+        rps = ['가위', '바위', '보']
+        print(' ----------- 1 ------------')
+        if p == 1:
+            if c == 1:
+                res = f'플레이어: {rps[0]} , 컴퓨터: {rps[0]}, 결과: 무승부'
+            elif c == 2:
+                res = f'플레이어: {rps[0]} , 컴퓨터: {rps[1]}, 결과: 패배'
+            elif c == '3':
+                res = f'플레이어: {rps[0]} , 컴퓨터: {rps[2]}, 결과: 승리'
+        elif p == 2:
+            if c == 1:
+                res = '승리'
+            elif c == 2:
+                res = '무승부'
+            elif c == 3:
+                res = '패배'
+        elif p == 3:
+            if c == 1:
+                res = '패배'
+            elif c == 2:
+                res = '승리'
+            elif c == 3:
+                res = '무승부'
+        else:
+            res = '1~3 입력'
+        print(res)
+        print(' ----------- 2 ------------')
+        if p == c:
+            res = f'플레이어:{rps[p - 1]}, 컴퓨터:{rps[c - 1]}, 결과:무승부'
+        elif p - c == 1 or p - c == -2:
+            res = f'플레이어:{rps[p - 1]}, 컴퓨터:{rps[c - 1]}, 결과:승리'
+        elif p - c == -1 or p - c == 2:
+            res = f'플레이어:{rps[p - 1]}, 컴퓨터:{rps[c - 1]}, 결과:패배'
+        else:
+            res = '1~3 입력'
+        print(res)
+
+    def quiz04leap(self):
+        pass
+
+    def quiz05grade(self):
+        kor = myRandom(0,100)
+        eng = myRandom(0, 100)
+        math = myRandom(0, 100)
+        sum = self.sum(kor, eng, math)
+        avg = self.agv(kor, eng, math)
+        grade = self.getGrade()
+        passChk = self.passChk()
+        return [sum, avg, grade, passChk]
+
+    def sum(self):
+        return self.kor + self.eng + self.math
+
+    def avg(self):
+        return self.kor + self.eng + self.math / 3
+
+    def grade(self):
+        pass
+
+    def passChk(self):  # 60점이상이면 합격
+        pass
+
+    def quiz06memberChoice(self):
+        members = ['홍정명', '노홍주', '전종현', '정경준', '양정오',
+                   "권혜민", "서성민", "조현국", "김한슬", "김진영",
+                   '심민혜', '권솔이', '김지혜', '하진희', '최은아',
+                   '최민서', '한성수', '김윤섭', '김승현',
+                   "강 민", "최건일", "유재혁", "김아름", "장원종"]
+        return members[myRandom(0, 23)]
+
+    def quiz07lotto(self):
+        pass
+
+    def quiz08bank(self):  # 이름, 입금, 출금만 구현
+        pass
+
+    def quiz09gugudan(self):  # 책받침구구단
+        pass
+
+
+
+
+
+
+
+
+
+
--- hello/quiz20.py
+++ hello/quiz20.py
@@ -1,178 +1,27 @@
 import random
 
-if __name__ == '__main__':
-    while 1:
-        menu = input('10버블 11삽입 12선택 13퀵 14병합 15매직 16지그재그 17직각별 18정삼각별 19예약')
-        if menu == '0':
-            break
-        elif menu == '1':
-            q1 = Quiz01Calculator(int(input('첫번째 수')), int(input('두번째 수')))
-            print(f'{q1.num1} + {q1.num2} = {q1.add()}')
-        elif menu == '2':
-            member = Member()
-            q2 = Quiz02Bmi()
-            member.name = input('이름 : ')
-            member.height = float(input('키 : '))
-            member.weight = float(input('몸무게 : '))
-            res = q2.getBmi(member)
-            print(f'이름: {member.name}, 키: {member.height}, '
-                  f'몸무게: {member.weight}, BMI상태: {res} ')
-        elif menu == '3':
-            q3 = Quiz03Grade()
-        elif menu == '4':
-            q4 = Quiz04GradeAuto()
-            for i in ['김유신', '강감찬', '유관순', '윤봉길', '신사임당']:
-                print(i)
-            kor = int(input('국어 : '))
-            eng = int(input('영어 : '))
-            math = int(input('수학 : '))
-            # grade =Grade(name,kor,eng,math)
-            # print(f'{name}님의 국어{kor} 영어{eng} 수학{math} 합계{grade.sum()} 평균{grade.avg()}')
-        elif menu == '5':
-            print(Quiz05Dice.cast())
-        elif menu == '6':
-            q6 = None
-        elif menu == '7':
-            q7 = Quiz07RandomChoice()
-            print(q7.chooseMember())
-        elif menu == '8':
-            q8 = Quiz08Rps(1) # 가위 1 바위 2 보 3
-            print(q8.game())
-    def __init__(self, num1, op, num2):
-        self.num1 = num1
-        self.op = op
-        self.num2 = num2
 
-    def calc(self):
-        self.add()
-        self.sub()
+class Quiz20:
 
-    def add(self):
-        return self.num1 + self.num2
-    def sub(self):
-        pass
+    def quiz20list(self) -> str: return None
 
-class Quiz02Bmi(object):
-    @staticmethod
-    def getBmi(member):
-        this = member
-        bmires =this.weight/(this.height*this.height)*10000
-        if bmires > 25:
-            return f'비만'
-        elif bmires > 23:
-            return f'과체중'
-        elif bmires > 18.5:
-            return f'정상'
-        else:
-            return f'저체중'
+    def quiz21tuple(self) -> str: return None
 
-class Quiz03Grade(object):
-    def __init__(self, name,kor,eng,math):
-        self.name = name
-        self.kor = kor
-        self.eng = eng
-        self.math = math
-    def sum(self):
-        return self.kor+self.eng+self.math
-    def avg(self):
-        return self.kor+self.eng+self.math /3
+    def quiz22dict(self) -> str: return None
 
-class Quiz04GradeAuto(object):
-    def __init__(self, name, kor, eng, math):
-        self.name = name
-        self.kor = kor
-        self.eng = eng
-        self.math = math
+    def quiz23listcom(self) -> str: return None
 
-    def sum(self):
-        return self.kor + self.eng + self.math
-    def avg(self):
-        return self.kor + self.eng + self.math / 3
-    def getGrade(self):
-        pass
-    def chkPass(self): # 60점이상이면 합격
-        pass
+    def quiz24zip(self) -> str: return None
 
-def myRandom(start, end):
-    return random.randint(start, end)
+    def quiz25dictcom(self) -> str: return None
 
-class Quiz05Dice(object):
-    @staticmethod
-    def cast():
-        return myRandom(1, 6)
+    def quiz26map(self) -> str: return None
 
-class Quiz07RandomChoice(object):
-    def __init__(self): # 803호에서 랜덤으로 1명 이름 추출
-        self.members = ['홍정명', '노홍주', '전종현', '정경준', '양정오',
-                        "권혜민", "서성민", "조현국", "김한슬", "김진영",
-                        '심민혜' , '권솔이', '김지혜' , '하진희' , '최은아',
-                        '최민서', '한성수', '김윤섭', '김승현',
-                        "강 민", "최건일", "유재혁", "김아름", "장원종"]
-    def chooseMember(self):
-        return self.members[myRandom(0, 23)]
+    def quiz27(self) -> str: return None
 
-class Quiz08Rps(object):
-    def __init__(self, player):
-        self.player = player
-        self.computer = myRandom(1,3)
+    def quiz28(self) -> str: return None
 
-    def game(self):
-        c = self.computer
-        p = self.player
-        # 1 가위 2  바위 3 보
-        rps = ['가위', '바위', '보']
-        '''if p == 1:
-            if c == 1:
-                res = f'플레이어: {rps[0]} , 컴퓨터: {rps[0]}, 결과: 무승부'
-            elif c == 2:
-                res = f'플레이어: {rps[0]} , 컴퓨터: {rps[1]}, 결과: 패배'
-            elif c == '3':
-                res = f'플레이어: {rps[0]} , 컴퓨터: {rps[2]}, 결과: 승리'
-        elif p == 2:
-            if c == 1:
-                res = '승리'
-            elif c == 2:
-                res = '무승부'
-            elif c == 3:
-                res = '패배'
-        elif p == 3:
-            if c == 1:
-                res = '패배'
-            elif c == 2:
-                res = '승리'
-            elif c == 3:
-                res = '무승부'
-        else:
-            res = '1~3 입력'
-
-        return res'''
-        if p == c:
-            res = f'플레이어:{rps[p - 1]}, 컴퓨터:{rps[c - 1]}, 결과:무승부'
-        elif p - c == 1 or p -c  == -2:
-            res = f'플레이어:{rps[p - 1]}, 컴퓨터:{rps[c - 1]}, 결과:승리'
-        elif p - c == -1 or p - c == 2:
-            res = f'플레이어:{rps[p - 1]}, 컴퓨터:{rps[c - 1]}, 결과:패배'
-        else:
-            res = '1~3 입력'
-        return res
-class Quiz09GetPrime(object):
-    def __init__(self):
-        pass
-class Quiz10LeapYear(object):
-    def __init__(self):
-        pass
-class Quiz11NumberGolf(object):
-    def __init__(self):
-        pass
-class Quiz12Lotto(object):
-    def __init__(self):
-        pass
-class Quiz13Bank(object): # 이름, 입금, 출금만 구현
-    def __init__(self):
-        pass
-class Quiz14Gugudan(object): # 책받침구구단
-    def __init__(self):
-        pass
+    def quiz29(self) -> str: return None
 
 
 
