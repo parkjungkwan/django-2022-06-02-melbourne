a6b81f1fe11ad1812c5e9efed9edb78f7c1db738
parkjungkwan <pakjkwan@gmail.com>
2022-03-10 15:48:21 +0900

    15

--- hello/quiz20.py
+++ hello/quiz20.py
@@ -26,31 +26,38 @@ class Quiz20:
         url = 'https://music.bugs.co.kr/chart/track/realtime/total'
         html_doc = urlopen(url)
         soup = BeautifulSoup(html_doc, 'lxml') # html.parser vs lxml
+        ls1 = self.find_music(soup, 'title')
+        ls2 = self.find_music(soup, 'artist')
+        dict = {}
+        for i in range(0, len(ls1)):
+            print(type(f'타입: {ls1[i]}'))
+            dict[ls1[i]] = ls2[i]
+        print(dict)
+        return None
 
-        # print(soup.prettify())
-        artists = soup.find_all('p', {'class':'artist'})
+    
+
+    def print_music_list(self, soup):
+        artists = soup.find_all('p', {'class': 'artist'})
         # print(type(artists)) # <class 'bs4.element.ResultSet'>
         artists = [i.get_text() for i in artists]
         # print(type(artists))
-        # print(''.join(i  for i in artists))
+        print(''.join(i  for i in artists))
         titles = soup.find_all('p', {'class': 'title'})
         titles = [i.get_text() for i in titles]
-        # print(''.join(i for i in titles))
-        '''
+        print(''.join(i for i in titles))
+    
+    def find_rank(self, soup):
         for i, j in enumerate(['artist', 'title']):
-            for i, j in enumerate(self.abc(soup, j)):
+            for i, j in enumerate(self.find_music(soup, j)):
                 print(f'{i}위 : {j}')
             print('#'*100)
-        '''
-        return None
 
     @staticmethod
-    def abc(soup, a) -> str:
-        titles = soup.find_all('p', {'class': a})
-        titles = [i.get_text() for i in titles]
-        return titles
-    
-    
+    def find_music(soup, cls_nm) -> []:
+        ls = soup.find_all('p', {'class': cls_nm})
+        return [i.get_text() for i in ls]
+
     def quiz25dictcom(self) -> str: return None
 
     def quiz26map(self) -> str: return None
