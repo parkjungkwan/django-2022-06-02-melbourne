2b945757d3133a6f87545b18c9ae9b71f2b750bb
parkjungkwan <pakjkwan@gmail.com>
2022-03-11 17:20:49 +0900

    17

--- hello/quiz30.py
+++ hello/quiz30.py
@@ -25,12 +25,29 @@ class Quiz30:
         ic| df:     0   1   2
                 0  97  57  52
                 1  56  83  80
-        '''
+    '''
     def quiz31_rand_2_by_3(self) -> str:
         
         return None
 
-    def quiz32(self) -> str: return None
+    '''
+            데이터프레임 문제 Q04.
+            국어, 영어, 수학, 사회 4과목을 시험치른 10명의 학생들의 성적표 작성.
+             단 점수 0 ~ 100이고 학생은 랜덤 알파벳 5자리 ID 로 표기
+             
+              ic| df4:        국어  영어  수학  사회
+                        lDZid  57  90  55  24
+                        Rnvtg  12  66  43  11
+                        ljfJt  80  33  89  10
+                        ZJaje  31  28  37  34
+                        OnhcI  15  28  89  19
+                        claDN  69  41  66  74
+                        LYawb  65  16  13  20
+                        QDBCw  44  32   8  29
+                        PZOTP  94  78  79  96
+                        GOJKU  62  17  75  49
+    '''
+    def quiz32_df_grade(self) -> str: return None
 
     def quiz33(self) -> str: return None
 
