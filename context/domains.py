52e5120d325777d95a7c821196f14c2ba678f902
parkjungkwan <pakjkwan@gmail.com>
2022-06-08 16:10:54 +0900

    16

--- context/domains.py
+++ context/domains.py
@@ -51,6 +51,30 @@ class Dataset:
     @label.setter
     def label(self,label): self._label = label
 
+@dataclass
+class File(object):
+    context: str
+    fname: str
+    dframe: object
+
+    @property
+    def context(self) -> str: return self._context
+
+    @context.setter
+    def context(self, context): self._context = context
+
+    @property
+    def fname(self) -> str: return self._fname
+
+    @fname.setter
+    def fname(self, fname): self._fname = fname
+
+    @property
+    def dframe(self) -> str: return self._dframe
+
+    @dframe.setter
+    def dframe(self, dframe): self._dframe = dframe
+
 class PrinterBase(metaclass=ABCMeta):
     @abstractmethod
     def dframe(self):
@@ -79,18 +103,24 @@ class Reader(ReaderBase):
     def new_file(self, file)-> str:
         return file.context + file.fname
 
-    def csv(self, fname)-> object:
-        return pd.read_csv(f'{self.new_file(fname)}.csv', encoding='UTF-8', thousands=',')
+    def csv(self, file)-> object:
+        return pd.read_csv(f'{self.new_file(file)}.csv', encoding='UTF-8', thousands=',')
 
-    def xls(self, fname, header, cols)-> object:
-        return pd.read_excel(f'{self.new_file(fname)}.xls', header=header, usecols=cols)
+    def xls(self, file, header, cols)-> object:
+        return pd.read_excel(f'{self.new_file(file)}.xls', header=header, usecols=cols)
 
-    def json(self, fname)-> object:
-        return pd.read_json(f'{self.new_file(fname)}.json', encoding='UTF-8')
+    def json(self, file)-> object:
+        return pd.read_json(f'{self.new_file(file)}.json', encoding='UTF-8')
 
     def gmaps(self) -> object:
         return googlemaps.Client(key='')
 
 class Printer(PrinterBase):
     def dframe(self, this):
-        pass
+        print('*' * 100)
+        print(f'1. Target type \n {type(this)} ')
+        print(f'2. Target column \n {this.columns} ')
+        print(f'3. Target top 1개 행\n {this.head(1)} ')
+        print(f'4. Target bottom 1개 행\n {this.tail(1)} ')
+        print(f'4. Target null 의 갯수\n {this.isnull().sum()}개')
+        print('*' * 100)
