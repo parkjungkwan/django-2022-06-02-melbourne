d187414b15b17116c4c2aaefaceb6022a9fee4a1
parkjungkwan <pakjkwan@gmail.com>
2022-06-15 15:49:58 +0900

    15

--- nlp/samsung_repot.py
+++ nlp/samsung_repot.py
@@ -12,10 +12,6 @@ import tweepy
 
 from context.domains import Reader, File
 '''
-자연어 처리에서 크롤링 등으로 얻어낸 코퍼스 데이터가 필요에 맞게 전처리되지 않은 상태라면, 
-해당 데이터를 사용하고자하는 용도에 맞게 토큰화(tokenization) & 정제(cleaning) & 정규화(normalization)하는 일을 하게 됩니다. 
-이번에는 그 중에서도 토큰화에 대해서 학습합니다.
-
 문장 형태의 문자 데이터를 전처리할 때 많이 사용되는 방법이다. 
 말뭉치(코퍼스 corpus)를 어떤 토큰의 단위로 분할하냐에 따라 
 단어 집합의 크기, 단어 집합이 표현하는 토크의 형태가 다르게 나타나며 
@@ -34,10 +30,12 @@ from context.domains import Reader, File
 코퍼스(corpus)는 단어들을 포함한다.
 임베딩(embedding)은 변환한 벡터들이 위치한 공간이다.
 
-단어(word)는 일반적으로 띄어쓰기나 줄바꿈과 같은 공백 문자(whitespace)로 나뉘어져 있는 문자열의 일부분이다.
+단어(word)는 일반적으로 띄어쓰기나 줄바꿈과 같은 공백 문자(whitespace)로 
+나뉘어져 있는 문자열의 일부분이다.
 단어를 벡터로 변환하는 경우 단어 임베딩(word embedding)이다. 
 각 문장을 벡터로 변환하는 경우 문장 임베딩(sentence embedding)이다. 
-단어 임베딩이란 앞서 말씀드린 바와 같이 이 각각 하나의 좌표를 가지도록 형성한 벡터공간이다.
+단어 임베딩이란 앞서 말씀드린 바와 같이 
+이 각각 하나의 좌표를 가지도록 형성한 벡터공간이다.
 
 1. Preprocessing : kr-Report_2018.txt 를 읽는다.
 2. Tokenization : 문자열(string)을 다차원 벡터(vector)로 변환
@@ -54,12 +52,9 @@ class Solution(Reader):
     def hook(self):
         def print_menu():
             print('0. Exit')
-            print('1. Preprocessing')
-            print('2. Tokenization.')
-            print('3. Token Embedding')
-            print('4. Document Embedding')
-            print('5. 2018년 삼성사업계획서를 분석해서 워드클라우드를 작성하시오.')
-            print('9. nltk 다운로드')
+            print('1. nltk 다운로드')
+            print('2. 전처리')
+            print('3. 워드클라우드')
             return input('메뉴 선택 \n')
 
         while 1:
@@ -67,20 +62,13 @@ class Solution(Reader):
             if menu == '0':
                 break
             elif menu == '1':
+                Solution.download()
+            elif menu == '2':
                 _ = self.preprocessing()
                 ic(_)
-            elif menu == '2':
-                self.tokenization()
             elif menu == '3':
-                self.token_embedding()
-            elif menu == '4':
-                self.document_embedding()
-            elif menu == '5':
                 self.draw_wordcloud()
-            elif menu == '6':
-                self.read_stopword()
-            elif menu == '9':
-                Solution.download()
+
     
     @staticmethod          
     def download():
@@ -97,38 +85,38 @@ class Solution(Reader):
         tokenizer = re.compile(r'[^ㄱ-힣]+')
         return tokenizer.sub(' ', texts)
 
-    def tokenization(self):
-        noun_tokens = []
+    def noun_embedding(self):
+        nouns = []
         tokens = word_tokenize(self.preprocessing())
-        # ic(tokens[:100])
         for i in tokens:
             pos = self.okt.pos(i)
             _ = [j[0] for j in pos if j[1] == 'Noun']
             if len(''.join(_)) > 1:
-                noun_tokens.append(' '.join(_))
-        texts = ' '.join(noun_tokens)
-        ic(texts[:100])
-        return texts
+                nouns.append(' '.join(_))
+        return nouns
 
-    def read_stopword(self):
+    def stopword_embedding(self):
         self.okt.pos("삼성전자 글로벌센터 전자사업부", stem=True)
         file = self.file
         file.fname = 'stopwords.txt'
         path = self.new_file(file)
         with open(path, 'r', encoding='utf-8') as f:
-            texts = f.read()
-        ic(texts)
-        return texts
+            stopwords = f.read()
+        return stopwords.split()
 
-    def token_embedding(self) -> []:
-        tokens = self.tokenization()
-        stopwords = self.read_stopword()
-        texts = [text for text in tokens if text not in stopwords]
-        return texts
+    def morphemes_embedding(self) -> []:
+        nouns = self.noun_embedding()
+        ic(nouns[:10])
+        stopwords = self.stopword_embedding()
+        ic(stopwords[:10])
+        morphemes = [text for text in nouns if text not in stopwords]
+        ic(morphemes[:10])
+        return morphemes
 
     def draw_wordcloud(self):
-        _ = self.token_embedding()
+        _ = self.morphemes_embedding()
         freqtxt = pd.Series(dict(FreqDist(_))).sort_values(ascending=False)
+        ic(type(freqtxt))
         ic(freqtxt)
         wcloud = WordCloud('./data/D2Coding.ttf', relative_scaling=0.2,
                            background_color='white').generate(" ".join(_))
